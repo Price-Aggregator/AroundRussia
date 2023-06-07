f2a3124b579fe7d07108375da8831b4a66ece34b
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-06-07 15:11:26 +0300

    fixes:header footer text components added styles

--- frontend/src/components/Text/Text.js
+++ frontend/src/components/Text/Text.js
@@ -2,27 +2,28 @@ import React from 'react';
 import iconTicket from '../../images/icon_ticket.svg';
 import iconSupport from '../../images/icon_support.svg';
 import iconCircle from '../../images/icon_circle.svg';
+import styles from './Text.module.css';
 
 function Text() {
 	return (
-		<div className="text">
-			<div className="text__item">
+		<div className={styles.text}>
+			<div className={styles.text__item}>
 				<img src={iconTicket} alt="ticket" />
-				<p className="text__content">
+				<p className={styles.text__content}>
 					Выбирайте направление и покупайте билет парой кликов, не выходя из
 					дома
 				</p>
 			</div>
-			<div className="text__item">
+			<div className={styles.text__item}>
 				<img src={iconSupport} alt="support" />
-				<p className="text__content">
+				<p className={styles.text__content}>
 					Компетентная служба поддержки всегда на связи и готова вам помочь в
 					любое время
 				</p>
 			</div>
-			<div className="text__item">
+			<div className={styles.text__item}>
 				<img src={iconCircle} alt="support" />
-				<p className="text__content">
+				<p className={styles.text__content}>
 					Наша система регулярно анализирует и обновляет данные о турах и ценах
 					на билеты
 				</p>
