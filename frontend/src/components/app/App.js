c3603b397f3cdbecca63d1017d3d8eea5e7bf4e9
Gleb Lyashenkov <glepka17@yandex.ru>
2023-06-07 15:49:17 +0300

    front: добавил секцию с вопросами и ответами

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -4,35 +4,37 @@ import './App.module.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Text from '../Text/Text';
+import Questions from '../Questions/Questions';
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: (
-			<>
-				<Header />
-				<Text />
-				<Footer />
-			</>
-		),
-	},
-	{
-		path: 'result',
-		element: (
-			<>
-				<Header />
-				<Footer />
-			</>
-		),
-	},
+  {
+    path: '/',
+    element: (
+      <>
+        <Header />
+        <Text />
+        <Questions />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: 'result',
+    element: (
+      <>
+        <Header />
+        <Footer />
+      </>
+    ),
+  },
 ]);
 
 function App() {
-	return (
-		<div className="App">
-			<RouterProvider router={router} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
