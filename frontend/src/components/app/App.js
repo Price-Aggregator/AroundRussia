543b4116966b8c456823567ba56bb19c86eb196d
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-06-07 17:58:10 +0300

    layout:search

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -5,36 +5,38 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Text from '../Text/Text';
 import Ticket from '../Ticket/Ticket';
+import Search from '../Search/Search';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <>
-        <Header />
-        <Text />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: 'result',
-    element: (
-      <>
-        <Header />
-        <Ticket price='2 000' />
-        <Footer />
-      </>
-    ),
-  },
+	{
+		path: '/',
+		element: (
+			<>
+				<Header />
+				<Search />
+				<Text />
+				<Footer />
+			</>
+		),
+	},
+	{
+		path: 'result',
+		element: (
+			<>
+				<Header />
+				<Ticket price="2 000" />
+				<Footer />
+			</>
+		),
+	},
 ]);
 
 function App() {
-  return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<RouterProvider router={router} />
+		</div>
+	);
 }
 
 export default App;
