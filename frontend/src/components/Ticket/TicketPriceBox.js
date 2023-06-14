b64e8b91015a2f0a21135e8b3f3fbeceda1a5195
Grechishnikov-Zarin Evgeniy <grecha-zarin@yandex.ru>
2023-06-14 22:59:00 +0300

    change: airplane pic, add: one transfer

--- frontend/src/components/Ticket/TicketPriceBox.js
+++ frontend/src/components/Ticket/TicketPriceBox.js
@@ -4,10 +4,13 @@ import styles from './Ticket.module.css'
 
 
 function TicketPriceBox({ price }) {
+  const priceString = price.toString().split('')
+  priceString.splice(-3, 0, ' ')
+
   return (
     <div className={styles.ticket__price}>
       <div className={styles.ticket__price_box}>
-        <span className={styles.price_text}>{price}
+        <span className={styles.price_text}>{priceString.join('')}
           <span className={styles.price_text}> ₽</span>
         </span>
         <button type='button' className={styles.button}>Выбрать билет</button>
