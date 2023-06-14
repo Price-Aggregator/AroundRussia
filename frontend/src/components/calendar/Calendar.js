410e79066731064d94939ccf615e03ab69fb510b
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-06-14 12:36:18 +0400

    moved the constant calendarData outside the component to prevent re-rendering

--- frontend/src/components/calendar/Calendar.js
+++ frontend/src/components/calendar/Calendar.js
@@ -4,25 +4,9 @@
 import styles from './Calendar.module.css';
 import Graph from './graph/Graph';
 import PropTypes from 'prop-types';
+import calendarData from '../../utils/constants';
 
 export default function Calendar({ departureCity, arrivalCity }) {
-	const tickets = [
-		{ _id: 1346, date: '25, чт', price: 2000, from: 'moscow', to: 'tver' },
-		{ _id: 24555, date: '26, пт', price: 3000, from: 'moscow', to: 'tver' },
-		{ _id: 35666, date: '27, сб', price: 3000, from: 'moscow', to: 'tver' },
-		{ _id: 456666, date: '28, вс', price: 4780, from: 'moscow', to: 'tver' },
-		{ _id: 55543, date: '29, пн', price: 8890, from: 'moscow', to: 'tver' },
-		{ _id: 64444, date: '30, вт', price: 9390, from: 'moscow', to: 'tver' },
-		{ _id: 7344443, date: '31, ср', price: 8490, from: 'moscow', to: 'tver' },
-		{ _id: 83332, date: '1, чт', price: 4490, from: 'moscow', to: 'tver' },
-		{ _id: 934344, date: '2, пт', price: 5590, from: 'moscow', to: 'tver' },
-		{ _id: 103424, date: '3, сб', price: 6590, from: 'moscow', to: 'tver' },
-		{ _id: 11243, date: '4, вс', price: 7590, from: 'moscow', to: 'tver' },
-		{ _id: 132433, date: '5, пн', price: 7000, from: 'moscow', to: 'tver' },
-		{ _id: 14422, date: '6, вт', price: 3000, from: 'moscow', to: 'tver' },
-		{ _id: 14422, date: '7, ср', price: 3000, from: 'moscow', to: 'tver' },
-	];
-
 	return (
 		<section className={styles.calendar}>
 			<h2 className={styles.calendar__title}>Календарь низких цен </h2>
@@ -39,7 +23,7 @@ export default function Calendar({ departureCity, arrivalCity }) {
 				</p>
 			</div>
 			<div className={styles.calendar__graph_box}>
-				<Graph tickets={tickets} />
+				<Graph tickets={calendarData} />
 				<div className={styles.calendar__month_group}>
 					<div className={styles.calendar__month_box}>
 						<div className={styles.calendar__month_icon} />
