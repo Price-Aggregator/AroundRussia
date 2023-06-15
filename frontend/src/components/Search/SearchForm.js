d72910036d4861c89c6eac030c731cdb7707d632
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-06-15 20:45:40 +0300

    feature: searchForm

--- frontend/src/components/Search/SearchForm.js
+++ frontend/src/components/Search/SearchForm.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Search.module.css';
 import calendar from '../../images/calendar.svg';
+import AutoComplete from '../Autocomplete/Autocomplete';
 
 function SearchForm() {
-	function handleSubmit(e) {
+	const [from, setFrom] = useState('');
+	const [to, setTo] = useState('');
+	const [when, setWhen] = useState('');
+	const [whenReturn, setWhenReturn] = useState('');
+	const [message, setMessage] = useState('');
+
+	const handleInput = async () => {
+		console.log('i work');
+		const list = fetch(`http://62.84.115.87:8000/api/v1/cities/`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then((res) => res.json());
+		return list;
+	};
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-	}
+		console.log('i work');
+		try {
+			const res = await fetch('http://127.0.0.1:8000/api/v1/airline', {
+				method: 'POST',
+				body: JSON.stringify({
+					from,
+					to,
+					when,
+					whenReturn,
+				}),
+			});
+			console.log(res);
+			// eslint-disable-next-line no-unused-vars
+			const resJson = await res.json();
+			localStorage.setItem('movies', JSON.stringify(resJson));
+			if (res.status === 200) {
+				setFrom('');
+				setTo('');
+				setWhen('');
+				setWhenReturn('');
+				setMessage('User created successfully');
+			} else {
+				setMessage('Some error occured');
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	return (
 		<>
 			<p className={styles.search__label}>Авиабилеты</p>
@@ -16,6 +62,23 @@ function SearchForm() {
 					placeholder="Откуда"
 					name="from"
 					required
+					onChange={(e) => setFrom(e.target.value)}
+					onInput={handleInput}
+					value={from || ''}
+				/>
+				<AutoComplete
+					suggestions={[
+						'Alligator',
+						'Bask',
+						'Crocodilian',
+						'Death Roll',
+						'Eggs',
+						'Jaws',
+						'Reptile',
+						'Solitary',
+						'Tail',
+						'Wetlands',
+					]}
 				/>
 				<input
 					type="text"
@@ -23,6 +86,8 @@ function SearchForm() {
 					placeholder="Куда"
 					name="to"
 					required
+					onChange={(e) => setTo(e.target.value)}
+					value={to || ''}
 				/>
 				<div className={styles.search__wrapper}>
 					<input
@@ -31,6 +96,8 @@ function SearchForm() {
 						placeholder="Когда"
 						name="when"
 						required
+						onChange={(e) => setWhen(e.target.value)}
+						value={when || ''}
 					/>
 					<img className={styles.search__image} alt="calendar" src={calendar} />
 				</div>
@@ -39,14 +106,16 @@ function SearchForm() {
 						type="text"
 						className={styles.search__input_right}
 						placeholder="Обратно"
-						name="return"
-						required
+						name="whenReturn"
+						onChange={(e) => setWhenReturn(e.target.value)}
+						value={whenReturn || ''}
 					/>
 					<img className={styles.search__image} alt="calendar" src={calendar} />
 				</div>
-        <button className={styles.search__button} type="submit">
+				<button className={styles.search__button} type="submit">
 					Найти
 				</button>
+				<div className="message">{message ? <p>{message}</p> : null}</div>
 			</form>
 		</>
 	);
