bc00e6d4b619871bffc8a08144587ae4f30fc5ea
Gleb Lyashenkov <glepka17@yandex.ru>
2023-06-15 18:27:43 +0300

    fix: переписал секцию вопросов

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -2,13 +2,14 @@ import React from "react";
 import Search from "../components/Search/Search";
 import Text from "../components/Text/Text";
 import Questions from "../components/Questions/Questions";
+import { faq } from "../utils/constants";
 
 function MainPage() {
   return (
     <>
       <Search />
       <Text />
-      <Questions />
+      <Questions data={faq} />
     </>
   )
 }
