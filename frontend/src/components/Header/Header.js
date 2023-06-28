096f2c4f4aa3bbd2f96bc5f3844819a9708a8689
Artem <lokomotivspb@gmail.com>
2023-06-28 04:39:32 -0400

    feat: filter connected to back

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,14 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import { HashLink as Link } from 'react-router-hash-link';
+import { useLocation, useNavigate } from 'react-router-dom';
 import logo from '../../images/logo.svg';
 import styles from './Header.module.css';
 
 function Header() {
+	const location = useLocation();
+	const navigate = useNavigate();
+
 	return (
 		<header className={styles.header}>
-			<img src={logo} className={styles.header__logo} alt="logo" />
-			<a className={styles.header__faq} href="#faq">
+			<Link
+				to="/"
+				onClick={location.pathname === '/' ? () => navigate(0) : null}
+			>
+				<img src={logo} className={styles.header__logo} alt="logo" />
+			</Link>
+			<Link
+				smooth
+				to={location.pathname === '/' ? '#faq' : '/#faq'}
+				className={styles.header__faq}
+			>
 				Вопросы и ответы
-			</a>
+			</Link>
 		</header>
 	);
 }
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -4,9 +4,17 @@ import { ticketsReducer } from './Tickets/slice';
 import { calendarReducer } from './Calendar/slice';
 import { searchFormReducer } from './SearchForm/slice';
 import { citiesReducer } from './Cities/slice';
+import { filtersReducer } from './Filter/slice';
 
 const store = configureStore({
-  reducer: { pagination: paginationReducer, tickets: ticketsReducer, calendar: calendarReducer, searchForm: searchFormReducer, cities: citiesReducer },
-})
+	reducer: {
+		filters: filtersReducer,
+		pagination: paginationReducer,
+		tickets: ticketsReducer,
+		calendar: calendarReducer,
+		searchForm: searchFormReducer,
+		cities: citiesReducer,
+	},
+});
 
-export default store
+export default store;
