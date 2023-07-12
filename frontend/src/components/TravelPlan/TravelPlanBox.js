5b37a689526f629caaaa250dc2d3fc60e68b58da
Grechishnikov-Zarin Evgeniy <grecha-zarin@yandex.ru>
2023-07-12 17:33:56 +0300

    add: time box

--- frontend/src/components/TravelPlan/TravelPlanBox.js
+++ frontend/src/components/TravelPlan/TravelPlanBox.js
@@ -1,5 +1,34 @@
 import React, { useState } from "react";
+import PropTypes from 'prop-types';
 import styles from './TravelPlan.module.css'
+import travelTicket from '../../images/travel-plan/ticket.png'
+import { hotel, event, plane } from "../../images/travel-plan";
+
+function EventBox({ type }) {
+  const image = {
+    plane,
+    hotel,
+    event
+  }
+
+  return (
+    <div className={styles.eventBox}>
+      <div className={styles.eventTimeAndIconBox}>
+        <div className={styles.eventTimeBox}>
+          {/* <span className={styles.eventTime}>14:00</span> */}
+          <span className={styles.eventTime}>14:00</span>
+        </div>
+        <img src={image[type]} alt="icon" className={styles.eventIcon} />
+      </div>
+      <div>
+        <h3>Перелёт</h3>
+      </div>
+      <div>
+        <img src={travelTicket} alt="Ticket" className={styles.eventImage} />
+      </div>
+    </div>
+  )
+}
 
 function TravelPlanBox() {
   const [wrap, setWrap] = useState(false)
@@ -9,10 +38,14 @@ function TravelPlanBox() {
       <h2 className={styles.date}>21 июля, Пн</h2>
       <button type="button" className={wrap ? styles.triangle : styles.triangleClose} onClick={() => setWrap(!wrap)}> </button>
     </ div>
-    {wrap && <div>
-      <h3>h3</h3>
-    </div>}
+    {wrap &&
+      <EventBox type="hotel" />
+    }
   </div>
+}
+
+EventBox.propTypes = {
+  type: PropTypes.string.isRequired
 }
 
 export default TravelPlanBox
---
+++ frontend/src/images/travel-plan/index.js
@@ -0,0 +1,6 @@
+import plane from './plane.svg'
+import hotel from './hotel.svg'
+import event from './event.svg'
+
+export { plane, hotel, event }
+
