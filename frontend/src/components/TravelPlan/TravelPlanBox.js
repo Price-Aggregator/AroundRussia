ac980c77817abb0de58beba1e9f9d00f2d9cb3fa
Grechishnikov-Zarin Evgeniy <grecha-zarin@yandex.ru>
2023-07-14 10:47:47 +0300

    add: travel plan event box

--- frontend/src/components/TravelPlan/TravelPlanBox.js
+++ frontend/src/components/TravelPlan/TravelPlanBox.js
@@ -4,7 +4,7 @@ import styles from './TravelPlan.module.css'
 import travelTicket from '../../images/travel-plan/ticket.png'
 import { hotel, event, plane } from "../../images/travel-plan";
 
-function EventBox({ type }) {
+function EventBox({ type, time, price, description, adress }) {
   const image = {
     plane,
     hotel,
@@ -15,15 +15,26 @@ function EventBox({ type }) {
     <div className={styles.eventBox}>
       <div className={styles.eventTimeAndIconBox}>
         <div className={styles.eventTimeBox}>
-          {/* <span className={styles.eventTime}>14:00</span> */}
-          <span className={styles.eventTime}>14:00</span>
+          {/* <span className={styles.eventTime}>14:00</span>  */}
+          <span className={styles.eventTime}>{time}</span>
         </div>
         <img src={image[type]} alt="icon" className={styles.eventIcon} />
       </div>
-      <div>
-        <h3>Перелёт</h3>
+      <div className={styles.eventSecondBox}>
+        <div className={styles.eventButtonBox}>
+          <h3 className={styles.eventHeaderText}>Перелёт</h3>
+          <button type="button" className={styles.eventButton}> </button>
+          <button type="button" className={styles.eventButtonTrash}> </button>
+        </div>
+        <div className={styles.eventDescriptionBox}>
+          <p className={styles.eventSmallText}>{adress}</p>
+          <p className={styles.eventSmallText}>{description}</p>
+          {price && <p className={styles.eventPriceText}>{price}
+            <span className={styles.eventPriceText}> ₽</span>
+          </p>}
+        </div>
       </div>
-      <div>
+      <div className={styles.eventImageBox}>
         <img src={travelTicket} alt="Ticket" className={styles.eventImage} />
       </div>
     </div>
@@ -38,14 +49,26 @@ function TravelPlanBox() {
       <h2 className={styles.date}>21 июля, Пн</h2>
       <button type="button" className={wrap ? styles.triangle : styles.triangleClose} onClick={() => setWrap(!wrap)}> </button>
     </ div>
-    {wrap &&
-      <EventBox type="hotel" />
+    {wrap && <div style={{ width: '100%' }}>
+      <EventBox type="hotel" time='14:00' adress="Аэропорт Шереметьево, терминал B" description="Заселение в 14:00. Спросить про вид на сад" price="18 000" />
+      <EventBox type="plane" time='14:00' />
+    </div>
     }
   </div>
 }
 
 EventBox.propTypes = {
-  type: PropTypes.string.isRequired
+  type: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  adress: PropTypes.string,
+  price: PropTypes.string,
+  description: PropTypes.string
+}
+
+EventBox.defaultProps = {
+  adress: '',
+  price: '',
+  description: ''
 }
 
 export default TravelPlanBox
