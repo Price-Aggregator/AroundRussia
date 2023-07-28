0cd679ec52fa71290480df02be80cbdda3a03d22
Gleb Lyashenkov <glepka17@yandex.ru>
2023-07-28 14:49:07 +0300

    fix: обработка ошибок при регистрации и входе

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -9,11 +9,11 @@ import styles from './Header.module.css';
 import LoginForm from '../LoginForm/LoginForm';
 import useClickOutside from '../../hooks/UseOutsideClick';
 import {
-  setUserToken,
-  setUserEmail,
-  yesAuth,
-  removeUser,
-  noAuth,
+	setUserToken,
+	setUserEmail,
+	yesAuth,
+	removeUser,
+	noAuth,
 } from '../../store/User/slice';
 
 import * as api from '../../utils/authApi';
@@ -21,219 +21,223 @@ import ResetPassword from '../LoginForm/ResetPassword';
 import Registration from '../LoginForm/Registration';
 
 function Header() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const [accountMenu, setAccountMenu] = useState(false);
+	const location = useLocation();
+	const navigate = useNavigate();
+	const [accountMenu, setAccountMenu] = useState(false);
 
-  const [loginForm, setLoginForm] = useState(false);
-  const [resetPasswordForm, setResetPasswordForm] = useState(false);
-  const [registrationForm, setRegistrationForm] = useState(false);
-  const dispatch = useDispatch();
-  const [isAuth, setIsAuth] = useState(false);
+	const [loginForm, setLoginForm] = useState(false);
+	const [resetPasswordForm, setResetPasswordForm] = useState(false);
+	const [registrationForm, setRegistrationForm] = useState(false);
+	const dispatch = useDispatch();
+	const [isAuth, setIsAuth] = useState(false);
 
-  const accountMenuRef = useRef(null);
-  const formsRef = useRef(null);
-  useClickOutside(accountMenuRef, () => {
-    setAccountMenu(false);
-  });
+	const accountMenuRef = useRef(null);
+	const formsRef = useRef(null);
+	useClickOutside(accountMenuRef, () => {
+		setAccountMenu(false);
+	});
 
-  const closeForms = () => {
-    setLoginForm(false);
-    setResetPasswordForm(false);
-    setRegistrationForm(false);
-  };
-  useClickOutside(formsRef, () => {
-    closeForms();
-  });
-  const openPasswordForm = () => {
-    closeForms();
-    setResetPasswordForm(true);
-  };
+	const closeForms = () => {
+		setLoginForm(false);
+		setResetPasswordForm(false);
+		setRegistrationForm(false);
+	};
+	useClickOutside(formsRef, () => {
+		closeForms();
+	});
+	const openPasswordForm = () => {
+		closeForms();
+		setResetPasswordForm(true);
+	};
 
-  const openRegistrationForm = () => {
-    closeForms();
-    setRegistrationForm(true);
-  };
-  useEffect(() => {
-    const token = localStorage.getItem('token');
+	const openRegistrationForm = () => {
+		closeForms();
+		setRegistrationForm(true);
+	};
+	useEffect(() => {
+		const token = localStorage.getItem('token');
 
-    if (token) {
-      setIsAuth(true);
-      dispatch(yesAuth());
-      dispatch(setUserToken({ token }))
-    }
-  }, []);
+		if (token) {
+			setIsAuth(true);
+			dispatch(yesAuth());
+			dispatch(setUserToken({ token }));
+		}
+	}, []);
 
-  const handleAuthorize = (data) => {
-    api
-      .authorize(data.email, data.password)
-      .then((token) => {
-        if (token) {
-          dispatch(
-            setUserToken({
-              token: token.auth_token,
-            })
-          );
-          dispatch(
-            setUserEmail({
-              email: data.email,
-            })
-          );
-          dispatch(yesAuth());
-          localStorage.setItem('token', token.auth_token);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(setIsAuth(true), closeForms());
-  };
+	const handleAuthorize = (data) => {
+		api
+			.authorize(data.email, data.password)
+			.then((token) => {
+				if (token) {
+					dispatch(
+						setUserToken({
+							token: token.auth_token,
+						})
+					);
+					dispatch(
+						setUserEmail({
+							email: data.email,
+						})
+					);
+					dispatch(yesAuth());
+					localStorage.setItem('token', token.auth_token);
+				}
+				setIsAuth(true);
+				closeForms();
+			})
+			.catch(() => {
+				alert('Неправильный логин или пароль');
+			});
+	};
 
-  const handleRegister = (email, password) => {
-    api
-      .register(email, password)
-      .then((user) => {
-        dispatch(
-          setUserEmail({
-            email: user.email,
-          })
-        );
-        handleAuthorize({ email, password });
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(closeForms());
-  };
+	const handleRegister = (email, password) => {
+		api
+			.register(email, password)
+			.then((user) => {
+				console.log(user);
+				dispatch(
+					setUserEmail({
+						email: user.email,
+					})
+				);
+				handleAuthorize({ email, password });
+				closeForms();
+			})
+			.catch(() => {
+				alert(
+					'Ваш пароль слишком простой, либо похож на email, либо пользователь с такими данными уже сущестует'
+				);
+			});
+	};
 
-  const handleSignOut = () => {
-    localStorage.removeItem('token');
-    dispatch(removeUser());
-    dispatch(noAuth());
-    setIsAuth(false);
-  };
+	const handleSignOut = () => {
+		localStorage.removeItem('token');
+		dispatch(removeUser());
+		dispatch(noAuth());
+		setIsAuth(false);
+	};
 
-  const handleResetPassword = (email) => {
-    api
-      .resetPassword(email)
-      .then(alert('Ссылка для восстановления отправлена на почту'))
-      .finally(closeForms());
-  };
+	const handleResetPassword = (email) => {
+		api
+			.resetPassword(email)
+			.then(alert('Ссылка для восстановления отправлена на почту'))
+			.finally(closeForms());
+	};
 
-  return (
-    <header className={styles.header}>
-      <Link
-        to="/"
-        onClick={location.pathname === '/' ? () => navigate(0) : null}
-      >
-        <img src={logo} className={styles.header__logo} alt="logo" />
-      </Link>
-      <div className={styles.header__linkContainer}>
-        <Link
-          smooth
-          to={location.pathname === '/' ? '#faq' : '/#faq'}
-          className={styles.header__link}
-        >
-          Вопросы и ответы
-        </Link>
-        <Link smooth to="/diary" className={styles.header__link}>
-          Дневник путешественника
-        </Link>
+	return (
+		<header className={styles.header}>
+			<Link
+				to="/"
+				onClick={location.pathname === '/' ? () => navigate(0) : null}
+			>
+				<img src={logo} className={styles.header__logo} alt="logo" />
+			</Link>
+			<div className={styles.header__linkContainer}>
+				<Link
+					smooth
+					to={location.pathname === '/' ? '#faq' : '/#faq'}
+					className={styles.header__link}
+				>
+					Вопросы и ответы
+				</Link>
+				<Link smooth to="/diary" className={styles.header__link}>
+					Дневник путешественника
+				</Link>
 
-        <button
-          type="button"
-          className={styles.header__accountButton}
-          onClick={() => {
-            setAccountMenu(!accountMenu);
-            // setIsLogged(!isLogged);
-          }}
-        >
-          <div
-            className={
-              isAuth
-                ? classNames(
-                  styles.header__accountLogo,
-                  styles.header__accountLogo_active
-                )
-                : styles.header__accountLogo
-            }
-          />
-        </button>
+				<button
+					type="button"
+					className={styles.header__accountButton}
+					onClick={() => {
+						setAccountMenu(!accountMenu);
+						// setIsLogged(!isLogged);
+					}}
+				>
+					<div
+						className={
+							isAuth
+								? classNames(
+										styles.header__accountLogo,
+										styles.header__accountLogo_active
+								  )
+								: styles.header__accountLogo
+						}
+					/>
+				</button>
 
-        {accountMenu &&
-          (!isAuth ? (
-            <div className={styles.header__accountMenu} ref={accountMenuRef}>
-              <button
-                type="button"
-                className={styles.header__accountMenuButton}
-                onClick={() => {
-                  setLoginForm(true);
-                  setAccountMenu(false);
-                }}
-              >
-                Войти
-              </button>
-              <button
-                type="button"
-                className={classNames(
-                  styles.header__accountMenuButton,
-                  styles.header__accountMenuButtonDark
-                )}
-                onClick={() => {
-                  setRegistrationForm(true);
-                  setAccountMenu(false);
-                }}
-              >
-                Зарегистрироваться
-              </button>
-            </div>
-          ) : (
-            <div
-              className={classNames(
-                styles.header__accountMenu,
-                styles.header__accountExitMenu
-              )}
-              ref={accountMenuRef}
-            >
-              <button
-                type="button"
-                className={styles.header__accountMenuButton}
-                onClick={() => {
-                  handleSignOut();
-                  setAccountMenu(false);
-                }}
-              >
-                Выйти
-              </button>
-            </div>
-          ))}
+				{accountMenu &&
+					(!isAuth ? (
+						<div className={styles.header__accountMenu} ref={accountMenuRef}>
+							<button
+								type="button"
+								className={styles.header__accountMenuButton}
+								onClick={() => {
+									setLoginForm(true);
+									setAccountMenu(false);
+								}}
+							>
+								Войти
+							</button>
+							<button
+								type="button"
+								className={classNames(
+									styles.header__accountMenuButton,
+									styles.header__accountMenuButtonDark
+								)}
+								onClick={() => {
+									setRegistrationForm(true);
+									setAccountMenu(false);
+								}}
+							>
+								Зарегистрироваться
+							</button>
+						</div>
+					) : (
+						<div
+							className={classNames(
+								styles.header__accountMenu,
+								styles.header__accountExitMenu
+							)}
+							ref={accountMenuRef}
+						>
+							<button
+								type="button"
+								className={styles.header__accountMenuButton}
+								onClick={() => {
+									handleSignOut();
+									setAccountMenu(false);
+								}}
+							>
+								Выйти
+							</button>
+						</div>
+					))}
 
-        <div className={styles.header__loginForm} ref={formsRef}>
-          {loginForm && (
-            <LoginForm
-              onClose={closeForms}
-              openPasswordForm={openPasswordForm}
-              openRegistrationForm={openRegistrationForm}
-              handleClick={handleAuthorize}
-            />
-          )}
-          {resetPasswordForm && (
-            <ResetPassword
-              onClose={closeForms}
-              handleClick={handleResetPassword}
-            />
-          )}
-          {registrationForm && (
-            <Registration
-              onClose={closeForms}
-              openPasswordForm={openPasswordForm}
-              handleClick={handleRegister}
-            />
-          )}
-        </div>
-      </div>
-    </header>
-  );
+				<div className={styles.header__loginForm} ref={formsRef}>
+					{loginForm && (
+						<LoginForm
+							onClose={closeForms}
+							openPasswordForm={openPasswordForm}
+							openRegistrationForm={openRegistrationForm}
+							handleClick={handleAuthorize}
+						/>
+					)}
+					{resetPasswordForm && (
+						<ResetPassword
+							onClose={closeForms}
+							handleClick={handleResetPassword}
+						/>
+					)}
+					{registrationForm && (
+						<Registration
+							onClose={closeForms}
+							openPasswordForm={openPasswordForm}
+							handleClick={handleRegister}
+						/>
+					)}
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
