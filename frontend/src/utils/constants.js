9b94f399a9ad01e0e15bebcb7679319320c6e3af
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-08-22 13:41:57 +0400

    fix base_url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -18,7 +18,7 @@ import { flight, hotel, activity, defaultImage } from '../images/travel-plan';
 import defaultPicture from '../images/picture_travellist_default.png';
 
 // const URL = 'http://localhost';
-const URL = 'https://5.42.220.171';
+const URL = 'https://go-to-travel.ru';
 
 const BASE_URL = `${URL}/api/v1`;
 const MEDIA_URL = `${URL}/media`;
