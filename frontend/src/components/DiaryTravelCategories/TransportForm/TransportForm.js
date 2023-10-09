b65a5b81b138a3ce9a5eb65278ad125f8bc69a58
Artem <lokomotivspb@gmail.com>
2023-10-09 17:27:29 -0300

    feat: functionality populated to TransportForm

--- frontend/src/components/DiaryTravelCategories/TransportForm/TransportForm.js
+++ frontend/src/components/DiaryTravelCategories/TransportForm/TransportForm.js
@@ -1,32 +1,202 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable prefer-promise-reject-errors */
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useState, useEffect } from 'react';
+// @ts-ignore
+// @typescript-eslint/ban-ts-comment
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useDropzone } from 'react-dropzone';
+import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import imageToBase64 from 'image-to-base64/browser';
 import styles from './TransportForm.module.css';
 import {
-	editTravel,
-	fetchAddEventStart,
-	fetchAddEventEnd,
-	fetchTravels,
+	fetchAddEvent,
 	fetchPatchEvent,
+	fetchTravels,
 } from '../../../store/Travels/slice';
-import { formatDate } from '../../../utils/utils';
 import { getUserToken } from '../../../store/User/selectors';
+import { formatDate } from '../../../utils/utils';
 import { TRAVEL_EVENT_EDIT } from '../../../utils/constants';
+import pdfIcon from '../../../images/pdf-icon.svg';
+
+const baseStyle = {
+	backgroundColor: '#fafafa',
+	borderStyle: 'solid',
+};
+
+const acceptStyle = {
+	borderColor: '#f8c747',
+	borderStyle: 'dashed',
+};
+
+const rejectStyle = {
+	borderColor: '#ff1744',
+};
+const loadFile = (file) =>
+	new Promise((res, rej) => {
+		const reader = new FileReader();
+		const base = {
+			name: file.name,
+			size: file.size,
+		};
+		reader.addEventListener('abort', (e) => rej(`File upload aborted:${e}`));
+		reader.addEventListener('error', (e) => rej(`File upload error: ${e}`));
+		reader.addEventListener(
+			'load',
+			() =>
+				res({
+					...base,
+					encoded: reader.result,
+				}),
+			false
+		);
+		reader.readAsDataURL(file);
+	});
 
 function TransportForm({ closeForm, actionName, eventId }) {
-	const [events, setEvents] = useState([]);
+	const [encodedFiles, setEncodedFiles] = useState([]);
+	const [previewFiles, setPreviewFiles] = useState([]);
+	const [medias, setMedias] = useState([]);
+
+	const onChange = (newFiles) => {
+		const newFilesWithPreview = newFiles.map((file) => ({
+			name: file.name,
+			preview: URL.createObjectURL(file),
+		}));
+		setPreviewFiles((prevFiles) => [...prevFiles, ...newFilesWithPreview]);
+	};
+
+	const removeFile = (file) => () => {
+		const updatedPreviewFiles = previewFiles.filter(
+			(f) => f.name !== file.name
+		);
+		setPreviewFiles(updatedPreviewFiles);
+
+		imageToBase64(file.preview)
+			.then((response) => {
+				const updatedEncodedFiles = encodedFiles.filter((encodedFile) => {
+					if (typeof encodedFile.encoded === 'string') {
+						return !encodedFile.encoded.includes(response.slice(0, 100));
+					}
+					return true;
+				});
+				setEncodedFiles(updatedEncodedFiles);
+			})
+			.catch((error) => {
+				console.log('error:', error);
+			});
+	};
+
+	useEffect(() => {
+		const updatedMedias = encodedFiles.map((file) => file.encoded);
+		setMedias(updatedMedias);
+	}, [encodedFiles]);
+
+	function renderFilePreviews(files) {
+		files.map((file) => file.preview.toLowerCase().endsWith('.pdf'));
+		return files.map((file) => (
+			<div key={file.name} className={styles.form__fileBoxContent}>
+				<button
+					type="button"
+					className={styles.dropzoneTrashBag}
+					onClick={removeFile(file)}
+				>
+					{' '}
+				</button>
+				{file.name.toLowerCase().endsWith('.pdf') ||
+				file.preview.toLowerCase().endsWith('.pdf') ? (
+					<img
+						src={pdfIcon}
+						alt={file.name}
+						className={styles.filePreviewPDF}
+					/>
+				) : (
+					<img
+						src={file.preview}
+						alt={file.name}
+						className={styles.filePreviewImage}
+					/>
+				)}
+				<p className={styles.form__filename}>{file.name}</p>
+			</div>
+		));
+	}
+
+	const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
+		onChange(
+			acceptedFiles.map((fl) =>
+				Object.assign(fl, {
+					preview: URL.createObjectURL(fl),
+					base64: localStorage.getItem('base64'),
+				})
+			)
+		);
+		acceptedFiles.forEach((file) =>
+			loadFile(file)
+				.then((encFile) => {
+					setEncodedFiles((prevEncodedFiles) => [...prevEncodedFiles, encFile]);
+					setMedias((prevMedias) => [...prevMedias, encFile.encoded]);
+				})
+				.catch((error) => console.log('error:', error))
+		);
+	}, []);
+
+	const {
+		acceptedFiles,
+		fileRejections,
+		getRootProps,
+		getInputProps,
+		isDragAccept,
+		isDragReject,
+	} = useDropzone({
+		accept: {
+			'image/*': ['.png', '.gif', '.jpeg', '.jpg'],
+			'application/pdf': ['.pdf'],
+		},
+		maxSize: 100000000,
+		multiple: true,
+		maxFiles: 9,
+		onDrop,
+		validator: (file) => {
+			if (encodedFiles.some((f) => f.name === file.name)) {
+				return {
+					code: 'name-dublicates',
+					message: `Файл ${file.name} уже добавлен`,
+				};
+			}
+			return null;
+		},
+	});
+
+	const style = useMemo(
+		() => ({
+			...baseStyle,
+			...(isDragAccept ? acceptStyle : {}),
+			...(isDragReject ? rejectStyle : {}),
+		}),
+		[isDragAccept, isDragReject]
+	);
+
+	const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+		<ul>
+			{errors.map((e) => (
+				<li key={e.code}>{e.message}</li>
+			))}
+		</ul>
+	));
+
 	const { travelId } = useParams();
 	const dispatch = useDispatch();
-	const travels = useSelector((state) => state.travels.travels);
 	const token = useSelector(getUserToken);
 
-	const [transportData, setTransportData] = useState({
+	const [eventData, setEventData] = useState({
 		category: 'flight',
 		eventName: '',
 		address: '',
@@ -37,52 +207,40 @@ function TransportForm({ closeForm, actionName, eventId }) {
 		endTime: null,
 		description: '',
 		price: '',
+		medias: [],
 	});
-
-	useEffect(() => {
-		let storedEvents;
-		try {
-			storedEvents = JSON.parse(localStorage.getItem('events'));
-		} catch (error) {
-			console.error('Error parsing stored events:', error);
-			storedEvents = undefined;
-		}
-		if (storedEvents) {
-			setEvents(storedEvents);
-		}
-	}, []);
 
 	const handleInputChange = (event) => {
 		const { name, value } = event.target;
-		setTransportData((prevData) => ({
+		setEventData((prevData) => ({
 			...prevData,
 			[name]: value,
 		}));
 	};
 
 	const handleStartDateChange = (date) => {
-		setTransportData((prevData) => ({
+		setEventData((prevData) => ({
 			...prevData,
 			startDate: date,
 		}));
 	};
 
 	const handleEndDateChange = (date) => {
-		setTransportData((prevData) => ({
+		setEventData((prevData) => ({
 			...prevData,
 			endDate: date,
 		}));
 	};
 
 	const handleStartTimeChange = (time) => {
-		setTransportData((prevData) => ({
+		setEventData((prevData) => ({
 			...prevData,
 			startTime: time,
 		}));
 	};
 
 	const handleEndTimeChange = (time) => {
-		setTransportData((prevData) => ({
+		setEventData((prevData) => ({
 			...prevData,
 			endTime: time,
 		}));
@@ -92,30 +250,32 @@ function TransportForm({ closeForm, actionName, eventId }) {
 		event.preventDefault();
 		let startTimeString = '';
 		let endTimeString = '';
-		if (transportData.startTime) {
+		if (eventData.startTime) {
 			// Check if eventData.startTime is defined
-			startTimeString = transportData.startTime.toLocaleTimeString([], {
+			startTimeString = eventData.startTime.toLocaleTimeString([], {
 				hour: '2-digit',
 				minute: '2-digit',
 			});
 		}
-		if (transportData.startTime) {
+		if (eventData.startTime) {
 			// Check if eventData.startTime is defined
-			endTimeString = transportData.startTime.toLocaleTimeString([], {
+			endTimeString = eventData.startTime.toLocaleTimeString([], {
 				hour: '2-digit',
 				minute: '2-digit',
 			});
 		}
+		eventData.medias = medias;
 		const newEvent = {
-			startDate: formatDate(transportData.startDate),
-			category: transportData.category,
+			startDate: formatDate(eventData.startDate),
+			category: eventData.category,
 			startTime: startTimeString,
-			endDate: formatDate(transportData.endDate),
+			endDate: formatDate(eventData.endDate),
 			endTime: endTimeString,
-			origin: transportData.address,
-			description: transportData.description,
-			price: transportData.price,
-			eventName: transportData.eventName,
+			origin: eventData.address,
+			description: eventData.description,
+			price: eventData.price,
+			eventName: eventData.eventName,
+			medias,
 		};
 
 		if (actionName === TRAVEL_EVENT_EDIT) {
@@ -153,7 +313,7 @@ function TransportForm({ closeForm, actionName, eventId }) {
 							type="text"
 							id="eventName"
 							name="eventName"
-							value={transportData.eventName}
+							value={eventData.eventName}
 							onChange={handleInputChange}
 							required
 						/>
@@ -167,7 +327,7 @@ function TransportForm({ closeForm, actionName, eventId }) {
 							type="text"
 							id="address"
 							name="address"
-							value={transportData.address}
+							value={eventData.address}
 							onChange={handleInputChange}
 							required
 						/>
@@ -181,7 +341,7 @@ function TransportForm({ closeForm, actionName, eventId }) {
 							type="text"
 							id="endAddress"
 							name="endAddress"
-							value={transportData.endAddress}
+							value={eventData.endAddress}
 							onChange={handleInputChange}
 							required
 						/>
@@ -195,7 +355,7 @@ function TransportForm({ closeForm, actionName, eventId }) {
 								<DatePicker
 									className={`${styles.form__input} ${styles.form__input_date}`}
 									id="startDate"
-									selected={transportData.startDate}
+									selected={eventData.startDate}
 									onChange={handleStartDateChange}
 									dateFormat="dd.MM.yyyy"
 									placeholderText=""
@@ -211,7 +371,7 @@ function TransportForm({ closeForm, actionName, eventId }) {
 								<DatePicker
 									className={`${styles.form__input} ${styles.form__input_date}`}
 									id="startTime"
-									selected={transportData.startTime}
+									selected={eventData.startTime}
 									onChange={handleStartTimeChange}
 									showTimeSelect
 									showTimeSelectOnly
@@ -233,7 +393,7 @@ function TransportForm({ closeForm, actionName, eventId }) {
 								<DatePicker
 									className={`${styles.form__input} ${styles.form__input_date}`}
 									id="endDate"
-									selected={transportData.endDate}
+									selected={eventData.endDate}
 									onChange={handleEndDateChange}
 									dateFormat="dd.MM.yyyy"
 									placeholderText=""
@@ -249,7 +409,7 @@ function TransportForm({ closeForm, actionName, eventId }) {
 								<DatePicker
 									className={`${styles.form__input} ${styles.form__input_date}`}
 									id="endTime"
-									selected={transportData.endTime}
+									selected={eventData.endTime}
 									onChange={handleEndTimeChange}
 									showTimeSelect
 									showTimeSelectOnly
@@ -271,7 +431,7 @@ function TransportForm({ closeForm, actionName, eventId }) {
 							type="text"
 							id="description"
 							name="description"
-							value={transportData.description}
+							value={eventData.description}
 							onChange={handleInputChange}
 						/>
 					</div>{' '}
@@ -284,33 +444,36 @@ function TransportForm({ closeForm, actionName, eventId }) {
 							type="text"
 							id="price"
 							name="price"
-							value={transportData.price}
+							value={eventData.price}
 							onChange={handleInputChange}
 						/>
 					</div>{' '}
 					<div className={styles.form__labelBox}>
-						<label htmlFor="title" className={styles.form__labelText}>
+						<label htmlFor="media" className={styles.form__labelText}>
 							Прикрепите фото, документы, билеты
 						</label>
-						<div className={styles.form__files}>
-							<div className={styles.form__fileBox}>
-								<button
-									className={`${styles.form__button} ${styles.form__button_addFile}`}
-									type="button"
-									onClick={() => console.log('click')}
+						<div className={styles.form__filesContainer} id="media">
+							<div className={styles.form__filesContainer} id="media">
+								<div
+									{...getRootProps({ style })}
+									className={styles.form__dropzone}
 								>
-									+ Файл
-								</button>
-							</div>{' '}
-							<div className={styles.form__fileBox}>
-								<button
-									className={`${styles.form__button} ${styles.form__button_addFile}`}
-									type="button"
-									onClick={() => console.log('click')}
-								>
-									+ Файл
-								</button>
-							</div>{' '}
+									<input {...getInputProps()} />
+									<div className={styles.form__fileBox}>
+										<button
+											className={`${styles.form__button} ${styles.form__button_addFile}`}
+											type="button"
+										>
+											<span>+ Файл</span>
+										</button>
+									</div>
+								</div>
+								{isDragReject && <p>только картинки и PDF, пожалуйста</p>}
+								{fileRejections && <p>{fileRejectionItems}</p>}
+								<div className={styles.form__attachments}>
+									{renderFilePreviews(previewFiles)}
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
